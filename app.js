54afc4fabf514d7984be606a550555e3cd2fcc2a
Josh Foster <josh@allenb.com>
2017-06-08 16:07:42 -0500

    misc

--- app.js
+++ app.js
@@ -71,7 +71,7 @@ app.use(function(req, res, next) {
 
 //below was added on 6/7 from various online articles
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 
 
 app.listen(PORT, () => {
